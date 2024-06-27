5e1a03d8452e7e985acae298fe9bba0d4c16e7c9
SrGesus <108523575+SrGesus@users.noreply.github.com>
2024-06-27 19:28:20 +0100

    Adding db dynamic dyspatch

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,61 +1,22 @@
-use sqlx::{
-    any::{install_default_drivers, AnyArguments, AnyRow}, query::{self, Query}, sqlite::SqliteConnectOptions, AnyPool, Arguments, Database, IntoArguments
-};
+use futures::future::BoxFuture;
+use sqlx::{database::HasArguments, query::Query, Error};
 
-use std::env;
+const DEFAULT_DB: &str = "sqlite:db.sqlite";
 
-pub struct DataBase {
-    pool: sqlx::AnyPool,
+mod sqlite;
+pub use sqlite::SqliteDB as SqliteDB;
+
+pub async fn get_database() -> Result<Box<dyn Db<impl sqlx::Database>>, Error> {
+  let url = std::env::var("DATABASE_URL").unwrap_or_else(|_| DEFAULT_DB.to_string());
+  Ok(Box::new(SqliteDB::new(&url).await?))
 }
 
-const DEFAULT_DB: &str = "sqlite:./db.sqlite";
+pub trait Db<DB: sqlx::Database> {
+  fn query<'q>(
+    &'q self,
+    query: Query<'q, DB, <DB as HasArguments<'q>>::Arguments>,
+    // ) -> Pin<Box<dyn futures::Future<Output = Result<Vec<DB::Row>, Error>> + Send + 'q>>;
+  ) -> BoxFuture<Result<Vec<DB::Row>, Error>>;
 
-impl DataBase {
-    pub async fn change_db(&mut self, url: &str) -> Result<(), sqlx::Error> {
-        self.pool = AnyPool::connect(url).await?;
-        Ok(())
-    }
-
-    pub async fn new() -> Result<Self, sqlx::Error> {
-        install_default_drivers();
-        let url = env::var("DATABASE_URL").unwrap_or(DEFAULT_DB.to_string());
-        let pool: AnyPool = AnyPool::connect(&url)
-            .await
-            .unwrap_or(AnyPool::connect(DEFAULT_DB).await?);
-        Ok(DataBase { pool })
-    }
-
-    pub async fn query<'q> (
-        &self,
-        query: Query<'q, sqlx::any::Any, sqlx::any::AnyArguments<'q, >>,
-    ) -> Result<Vec<AnyRow>, sqlx::Error> {
-        query.fetch_all(&self.pool).await
-    }
-
-    pub async fn init(&self) {
-        let query: Query<'_, sqlx::Any, AnyArguments<'_>> = sqlx::query!(
-            "CREATE TABLE IF NOT EXISTS currency (
-              id SERIAL PRIMARY KEY
-            )",
-        );
-        self.query(query).await.unwrap();
-
-    //   self.conn.execute(
-    //     "CREATE TABLE IF NOT EXISTS currency (
-    //       id SERIAL PRIMARY KEY,
-    //       name TEXT NOT NULL,
-    //       symbol CHAR(3) NOT NULL,
-    //     )",
-    //     [],
-    //   );
-    //   self.conn.execute(
-    //     "CREATE TABLE IF NOT EXISTS account (
-    //       id SERIAL PRIMARY KEY,
-    //       name TEXT NOT NULL,
-    //       type TEXT NOT NULL,
-    //       currency TEXT NOT NULL REFERENCES currency(name),
-    //     )",
-    //     [],
-    //   );
-    }
+  fn init(&self) -> BoxFuture<Result<(), Error>>;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 use axum::{routing::get, Router};
 
-use crate::db::DataBase;
+pub mod api;
+pub mod db;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+  tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(root));
+  // Configure and initialize the database
+  let db = db::get_database().await.unwrap();
+  db.init().await.unwrap();
 
-    println!("Hello, world!");
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+  let app = Router::new().route("/", get(root));
+
+  println!("Hello, world!");
+  // run our app with hyper, listening globally on port 3000
+  let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+  axum::serve(listener, app).await.unwrap();
 }
 
 async fn root() -> &'static str {
-    tracing::info!("Yippie!");
-    "Hello world!"
+  tracing::info!("Yippie!");
+  "Hello world!"
 }
