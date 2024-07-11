a7b6725ea935b1dd78157007b4f8642cfd3f26d0
SrGesus <108523575+SrGesus@users.noreply.github.com>
2024-07-12 00:32:23 +0100

    Added Api Error type

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -2,76 +2,89 @@ use axum::{body::Body, http::StatusCode, response::IntoResponse};
 use sea_orm::DbErr;
 use serde::Serializer;
 
+use thiserror::Error;
+
 pub mod account;
 pub mod currency;
 pub mod plugin;
 
-#[derive(serde::Serialize)]
-pub enum Status {
-  Success,
-  Fail,
-  Error,
+#[derive(Error, Debug)]
+pub enum Error {
+  #[error("Internal server error. Check the logs for more information.")]
+  UnknownDbError(DbErr),
+  #[error("Could not find {0} with {1} = {2}")]
+  NoSuchEntity(&'static str, &'static str, String),
+  #[error("There already exists a {0} with {1} = {2}")]
+  DuplicateEntity(&'static str, &'static str, String),
+  #[error("{0}")]
+  InvalidInput(String),
+}
+
+impl Error {
+  pub fn status_code(&self) -> StatusCode {
+    match self {
+      Error::UnknownDbError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+      Error::NoSuchEntity(_, _, _) => StatusCode::BAD_REQUEST,
+      Error::DuplicateEntity(_, _, _) => StatusCode::CONFLICT,
+      Error::InvalidInput(_) => StatusCode::BAD_REQUEST,
+    }
+  }
+}
+
+impl Into<Response> for Error {
+  fn into(self) -> Response {
+    Response {
+      status: self.status_code(),
+      message: Some(self.to_string()),
+      data: serde_json::Value::Null,
+    }
+  }
+}
+
+impl IntoResponse for Error {
+  fn into_response(self) -> axum::response::Response<Body> {
+    Into::<Response>::into(self).into_response()
+  }
+}
+
+impl From<DbErr> for Error {
+  fn from(err: DbErr) -> Self {
+    Error::UnknownDbError(err)
+  }
 }
 
 #[derive(serde::Serialize)]
 pub struct Response {
-  #[serde(serialize_with = "code_to_status")]
-  status_code: u16,
-  status: Status,
+  #[serde(serialize_with = "serialize_status")]
+  status: StatusCode,
   message: Option<String>,
   data: serde_json::Value,
 }
 
-fn code_to_status<S>(value: &u16, serializer: S) -> Result<S::Ok, S::Error>
+fn code_to_status(value: &StatusCode) -> String {
+  if value.as_u16() < 400 {
+    "success".to_owned()
+  } else if value.as_u16() < 500 {
+    "fail".to_owned()
+  } else {
+    "error".to_owned()
+  }
+}
+
+fn serialize_status<S>(value: &StatusCode, serializer: S) -> Result<S::Ok, S::Error>
 where
   S: Serializer,
 {
-  // For demonstration, convert the string to uppercase before serialization
-  // let modified_value = value.to_uppercase();
-  // serializer.serialize_str(&modified_value)
-  if *value < 400 {
-    serializer.serialize_str("success")
-  } else if *value < 500 {
-    serializer.serialize_str("fail")
-  } else {
-    serializer.serialize_str("error")
-  }
+  serializer.serialize_str(&code_to_status(value))
 }
 
-// implement from arbitrary for ApiResponse
+// implement from arbitrary json for ApiResponse
 impl From<serde_json::Value> for Response {
   fn from(value: serde_json::Value) -> Self {
     Self {
-      status_code: StatusCode::OK.as_u16(),
-      status: Status::Success,
+      status: StatusCode::OK,
       message: None,
       data: value,
-    }
-  }
-}
-
-impl From<DbErr> for Response {
-  fn from(err: DbErr) -> Self {
-    let status_code = match err {
-      sea_orm::DbErr::RecordNotFound(_) => StatusCode::NOT_FOUND,
-      sea_orm::DbErr::RecordNotInserted => StatusCode::BAD_REQUEST,
-      sea_orm::DbErr::RecordNotUpdated => StatusCode::BAD_REQUEST,
-      sea_orm::DbErr::TryIntoErr { .. } => StatusCode::BAD_REQUEST,
-      sea_orm::DbErr::ConvertFromU64(_) => StatusCode::BAD_REQUEST,
-      sea_orm::DbErr::Type(_) => StatusCode::BAD_REQUEST,
-      sea_orm::DbErr::Json(_) => StatusCode::BAD_REQUEST,
-      _ => StatusCode::INTERNAL_SERVER_ERROR,
-    }
-    .as_u16();
-    Self {
-      status_code,
-      status: if status_code < 500 {
-        Status::Fail
-      } else {
-        Status::Error
-      },
-      message: Some(format!("Database error: {}", err)),
-      data: serde_json::Value::Null,
     }
   }
 }
@@ -80,7 +93,7 @@ impl IntoResponse for Response {
   fn into_response(self) -> axum::response::Response<Body> {
     let body = serde_json::to_string(&self).unwrap();
     axum::response::Response::builder()
-      .status(StatusCode::from_u16(self.status_code).unwrap())
+      .status(self.status)
       .header("Content-Type", "application/json")
       .body(Body::from(body))
       .unwrap()
