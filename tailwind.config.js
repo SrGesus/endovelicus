300c8821ffba717b617601d523519732531f982c
SrGesus <108523575+SrGesus@users.noreply.github.com>
2024-08-10 02:54:59 +0100

    Adding tailwind and daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.html.tera"],  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
