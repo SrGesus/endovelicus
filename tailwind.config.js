c174ace3a0e36bebf6ea85d76e823f3ffd63e302
SrGesus <108523575+SrGesus@users.noreply.github.com>
2024-08-13 22:52:07 +0100

    theme Local storage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html.tera"],  theme: {
+  content: ["./templates/**/*.html.tera", "./templates/**/*.html.tera"],
+  theme: {
     extend: {},
   },
   plugins: [
